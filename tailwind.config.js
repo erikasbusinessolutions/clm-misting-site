3f3ee27689f58c14f68aa0abc24fb92dfe4ec77b
erikasbusinessolutions <erikasbusinesssolutions@gmail.com>
2025-02-08 13:08:43 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#008FD5",
+        secondary: "#6C6C6C",
+        accent: "#FFA733",
+        highlight: "#B24364",
+        success: "#65C51B",
+      },
+    },
+  },
+  plugins: [],
+};
