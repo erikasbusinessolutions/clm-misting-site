452386a6ec6133a2a895c016d39eaf997ba02311
erikasbusinessolutions <erikasbusinesssolutions@gmail.com>
2025-02-08 13:19:06 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#008FD5",  // Brand Blue
-        secondary: "#6C6C6C", // Dark Gray
+        primary: "#008FD5",  // Blue
+        secondary: "#6C6C6C", // Gray
         accent: "#FFA733",   // Orange
         highlight: "#B24364", // Red/Pink
         success: "#65C51B",  // Green
