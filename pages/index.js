e5b578ac88bfa3b76d0d87c5fa823cd3b9b893a5
erikasbusinessolutions <erikasbusinesssolutions@gmail.com>
2025-02-08 12:55:42 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1 style={{ color: "#008FD5" }}>CLM Misting Solutions</h1>
+      <p>Protect your outdoor spaces with advanced mosquito misting systems.</p>
+      <button style={{ background: "#B24364", color: "white", padding: "10px 20px", border: "none" }}>
+        Request a Quote
+      </button>
+    </div>
+  );
+}
